12fcfc560b975cc4247748aee3144dc3861c5b16
DiSlord <dislord@mail.ru>
2020-07-03 23:57:27 +0300

    Small text fixes

--- nanovna.h
+++ nanovna.h
@@ -868,7 +868,7 @@ void SI4432_SetReference(int freq);
 // Speed profile definition
 #define START_PROFILE   systime_t time = chVTGetSystemTimeX();
 #define RESTART_PROFILE   time = chVTGetSystemTimeX();
-#define STOP_PROFILE    {char string_buf[12];plot_printf(string_buf, sizeof string_buf, "T:%06d", chVTGetSystemTimeX() - time);ili9341_drawstringV(string_buf, 1, 180);}
+#define STOP_PROFILE    {char string_buf[12];plot_printf(string_buf, sizeof string_buf, "%06d", chVTGetSystemTimeX() - time);ili9341_drawstring(string_buf, 0, FREQUENCIES_YPOS);}
 #define DELTA_TIME (time = chVTGetSystemTimeX() - time)
 // Macros for convert define value to string
 #define STR1(x)  #x
--- sa_core.c
+++ sa_core.c
@@ -1569,9 +1569,9 @@ float perform(bool break_on_operation, int i, uint32_t f, int tracking)     // M
             data_level == T_LEVEL_ABOVE))                                    // trigger level change
         goto wait;                                                           // get next rssi
 #ifdef __FAST_SWEEP__
-        if (i == 0 && setting.frequency_step == 0 && setting.spur == 0 && old_SI4432_step_delay == 0 && setting.repeat == 1 && setting.sweep_time_us < ONE_SECOND_TIME) {
-           SI4432_Fill(MODE_SELECT(setting.mode), 1);                       // fast mode possible to pre-fill RSSI buffer
-        }
+      if (setting.spur == 0 && old_SI4432_step_delay == 0 && setting.repeat == 1 && setting.sweep_time_us < 100*ONE_MS_TIME) {
+         SI4432_Fill(MODE_SELECT(setting.mode), 1);                       // fast mode possible to pre-fill RSSI buffer
+      }
 #endif
       if (setting.trigger == T_SINGLE)
         pause_sweep();                    // Trigger once so pause after this sweep has completed!!!!!!!
@@ -2198,7 +2198,7 @@ float my_round(float v)
   return v;
 }
 
-const char * const unit_string[] = { "dBm", "dBmV", "dBuV", "V", "W", "dBc", "dBc", "dBc", "Vc", "Wc" }; // unit + 5 is delta unit
+const char * const unit_string[] = { "dBm", "dBmV", "dB"S_MICRO"V", "V", "W", "dBc", "dBc", "dBc", "Vc", "Wc" }; // unit + 5 is delta unit
 
 static const float scale_value[]={50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20,10,5,2,1,0.5,0.2,0.1,0.05,0.02,0.01,0.005,0.002, 0.001,0.0005,0.0002, 0.0001};
 static const char * const scale_vtext[]= {"50000", "20000", "10000", "5000", "2000", "1000", "500", "200", "100", "50", "20","10","5","2","1","0.5","0.2","0.1","0.05","0.02","0.01", "0.005","0.002","0.001", "0.0005","0.0002","0.0001"};
@@ -2219,7 +2219,7 @@ void draw_cal_status(void)
     rounding  = true;
   const char * const unit = unit_string[setting.unit];
 
-  ili9341_fill(0, 0, OFFSETX, LCD_HEIGHT-1, 0x0000);
+  ili9341_fill(0, 0, OFFSETX, HEIGHT_NOSCROLL, 0x0000);
   if (MODE_OUTPUT(setting.mode)) {     // No cal status during output
     return;
   }
