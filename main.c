ad1ed4d47752900ccd19f1fb1519087b54d119f6
erikkaashoek <erik@kaashoek.com>
2023-07-01 14:11:55 +0200

    Update

--- main.c
+++ main.c
@@ -2866,7 +2866,7 @@ int main(void)
   if (max2871) {
     ULTRA_MAX_FREQ      = 6950000000ULL + config.overclock;        // Start of harmonic mode
     MAX_LO_FREQ         = 6000000000ULL + config.overclock;
-    MAX_ABOVE_IF_FREQ   = 4500000000ULL + config.overclock;         // Range to use for below IF
+    MAX_ABOVE_IF_FREQ   = 5050000000ULL + config.overclock;         // Range to use for below IF
     MIN_BELOW_IF_FREQ   = 2310000000ULL + config.overclock;         // Range to use for below IF
 
   } else {
