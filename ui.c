294ba13c0cdccdbea7016eaa270a2fa656e3b981
erikkaashoek <erik@kaashoek.com>
2023-12-24 11:25:37 +0100

    Reduced CSV output to 3 digits after the comma

--- ui.c
+++ ui.c
@@ -7717,10 +7717,10 @@ static void sa_save_file(uint8_t format) {
             else
               buf += plot_printf(buf, 100, "%U,", getFrequency(i));
           }
-          if (file_mask & 2)  buf += plot_printf(buf, 100, "%f", value(measured[TRACE_ACTUAL][i]));
-          if (file_mask & 4)  buf += plot_printf(buf, 100, " %f ", value(measured[TRACE_STORED][i]));
-          if (file_mask & 8)  buf += plot_printf(buf, 100, " %f ", value(measured[TRACE_STORED2][i]));
-          if (file_mask & 16) buf += plot_printf(buf, 100, " %f", value(measured[TRACE_TEMP][i]));
+          if (file_mask & 2)  buf += plot_printf(buf, 100, "%.3f", value(measured[TRACE_ACTUAL][i]));
+          if (file_mask & 4)  buf += plot_printf(buf, 100, " %.3f ", value(measured[TRACE_STORED][i]));
+          if (file_mask & 8)  buf += plot_printf(buf, 100, " %.3f ", value(measured[TRACE_STORED2][i]));
+          if (file_mask & 16) buf += plot_printf(buf, 100, " %.3f", value(measured[TRACE_TEMP][i]));
           buf += plot_printf(buf, 100, "\r\n");
           res = f_write(fs_file, (char *)spi_buffer, buf - (char *)spi_buffer, &size);
         }
